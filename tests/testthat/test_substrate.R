e37581a5489dd0bf1cebf6ffdf29876da65761f1
fawda123 <mbafs2012@gmail.com>
2019-07-31 09:34:12 -0700

    adding another example test

--- tests/testthat/test_substrate.R
+++ tests/testthat/test_substrate.R
@@ -1,7 +1,13 @@
 test_that('Checking PCT_RS.result', {
   
-  PCT_RS.result <- submet$PCT_RS.result
-  expect_equal(c(0, 0, 0, 0), PCT_RS.result)
+  result <- submet$PCT_RS.result
+  expect_equal(c(0, 0, 0, 0), result)
   
-  }
-)
+})
+
+test_that('Checking PCT_SB.result', {
+  
+  result <- submet$PCT_SB.result
+  expect_equal(c(7, 1, 0, 20), result)
+  
+})
