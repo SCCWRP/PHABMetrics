8a87959b76bdfbfeb69f29736191fb949774aa39
Robert Butler <r7butler@yahoo.com>
2019-08-23 16:36:42 -0700

    had to explicitly call package names for the sake of the checker in channelsinuosity function

--- R/channelsinuosity.R
+++ R/channelsinuosity.R
@@ -14,7 +14,7 @@ channelsinuosity <- function(data){
   # XSLOPE data ----------------------------------------------------------------------------------
   data_slope <- data %>%
     dplyr::group_by(id) %>%
-    arrange(id) %>% 
+    dplyr::arrange(id) %>% 
     dplyr::mutate(
       Result = dplyr::case_when(
         AnalyteName == 'Proportion' ~ Result/100, # convert % to proportion
