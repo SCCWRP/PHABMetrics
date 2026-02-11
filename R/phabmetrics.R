cc6dd295fbee2124de4197029dc3d61028ecc003
Addison Grant <10889879+addison-grant@users.noreply.github.com>
2026-02-11 11:34:14 -0800

    Format code with Positron formatter
    Make the code more readable

--- R/phabmetrics.R
+++ R/phabmetrics.R
@@ -1,92 +1,121 @@
 #' Calculate all PHAB metrics
 #'
 #' @param data Input data
-#' 
+#'
 #' @export
-#' 
+#'
 #' @importFrom magrittr "%>%"
 #'
-#' @examples 
+#' @examples
 #' \dontrun{
 #' phabmetrics(sampdat)
 #' }
-phabmetrics <- function(data){
-  if (!("sampleagencycode" %in% tolower(names(data)))){
-    data <- data %>% 
+phabmetrics <- function(data) {
+  if (!("sampleagencycode" %in% tolower(names(data)))) {
+    data <- data %>%
       dplyr::mutate(
         SampleAgencyCode = 'Not Recorded'
       )
   }
-  
+
   # format input
   data <- phabformat(data)
-  
+
   # chkinp threw off values of sinuosity metrics by removing rows of data
   # We should probably let the checker application check the data so users (and us) are aware of any problems with their data
   data <- chkinp(data, purge = TRUE)
-  
+
   # calc metrics
-  metrics <- list(bankmorph(data), channelmorph(data), channelsinuosity(data),
-                  densiometer(data),  habitat(data), disturbance(data), flow(data),
-                  misc(data), bankstability(data), quality(data), ripveg(data),
-                  substrate(data), algae(data))
+  metrics <- list(
+    bankmorph(data),
+    channelmorph(data),
+    channelsinuosity(data),
+    densiometer(data),
+    habitat(data),
+    disturbance(data),
+    flow(data),
+    misc(data),
+    bankstability(data),
+    quality(data),
+    ripveg(data),
+    substrate(data),
+    algae(data)
+  )
 
-  # combine seprate metrics lists 
-  out <- purrr::map(metrics, function(x){
-
-    lnfrm <- x %>% 
-      as.data.frame(stringsAsFactors = FALSE) %>% 
-      tibble::rownames_to_column('phab_sampleid') %>% 
-      dplyr::mutate_if(is.numeric, as.character) %>% 
+  # combine seprate metrics lists
+  out <- purrr::map(metrics, function(x) {
+    lnfrm <- x %>%
+      as.data.frame(stringsAsFactors = FALSE) %>%
+      tibble::rownames_to_column('phab_sampleid') %>%
+      dplyr::mutate_if(is.numeric, as.character) %>%
       tidyr::gather('var', 'val', -phab_sampleid)
-    
+
     return(lnfrm)
-    
-  }) %>% 
-  do.call('rbind', .) %>% 
-  dplyr::mutate(
-    val = gsub('NaN', NA, val)
-  ) %>% 
-  tidyr::spread('var', 'val') %>% 
-  dplyr::mutate_if(
-    ~ !any(grepl('[a-z,A-Z]', .x)), as.numeric
+  }) %>%
+    do.call('rbind', .) %>%
+    dplyr::mutate(
+      val = gsub('NaN', NA, val)
+    ) %>%
+    tidyr::spread('var', 'val') %>%
+    dplyr::mutate_if(
+      ~ !any(grepl('[a-z,A-Z]', .x)),
+      as.numeric
     )
-  
-  
-  out <- out %>% 
+
+  out <- out %>%
     dplyr::left_join(
-      data %>% 
+      data %>%
         dplyr::mutate(
           phab_sampleid = dplyr::case_when(
             toupper(SampleAgencyCode) != 'NOT RECORDED' ~ paste(
-              StationCode, SampleDate, SampleAgencyCode, sep = "_"
+              StationCode,
+              SampleDate,
+              SampleAgencyCode,
+              sep = "_"
             ),
-            toupper(SampleAgencyCode) == 'NOT RECORDED' ~ paste(StationCode, SampleDate, sep = "_"),
+            toupper(SampleAgencyCode) == 'NOT RECORDED' ~ paste(
+              StationCode,
+              SampleDate,
+              sep = "_"
+            ),
             TRUE ~ NA_character_
           )
         ) %>%
         dplyr::select(
-          phab_sampleid, StationCode, SampleDate, SampleAgencyCode
+          phab_sampleid,
+          StationCode,
+          SampleDate,
+          SampleAgencyCode
         ) %>%
-        dplyr::distinct(phab_sampleid,StationCode,SampleDate,SampleAgencyCode)
-      , by = 'phab_sampleid'
+        dplyr::distinct(
+          phab_sampleid,
+          StationCode,
+          SampleDate,
+          SampleAgencyCode
+        ),
+      by = 'phab_sampleid'
     )
-  
+
   out <- out %>%
     dplyr::select(
-      c(phab_sampleid,StationCode,SampleDate,SampleAgencyCode,
-      names(out)[which(!(names(out) %in% c('StationCode','SampleDate','SampleAgencyCode','phab_sampleid')))]
+      c(
+        phab_sampleid,
+        StationCode,
+        SampleDate,
+        SampleAgencyCode,
+        names(out)[which(
+          !(names(out) %in%
+            c('StationCode', 'SampleDate', 'SampleAgencyCode', 'phab_sampleid'))
+        )]
       )
     )
-  
+
   out <- longformat(out) %>% select(-phab_sampleid)
-  
+
   if ((out %>% dplyr::distinct(SampleAgencyCode) == 'Not Recorded') %>% all()) {
     out <- out %>%
       dplyr::select(-SampleAgencyCode)
   }
-  
-  
+
   return(out)
-  
 }
